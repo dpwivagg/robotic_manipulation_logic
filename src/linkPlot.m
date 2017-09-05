99e6bc5d01bd2c1325983110a92f564ddf455d1d
Nick Benoit <nbenoit@wpi.edu>
2017-09-05 10:59:01 -0400

    Changed plot from point to line

--- src/linkPlot.m
+++ src/linkPlot.m
@@ -2,16 +2,33 @@
 % plots the position of the link
 function linkPlot(q)
 
+% Add axes to the figure
+axes;
+% Hold on to objects in the axes
+hold on;
+% Lock aspect ratios equal
+axis equal;
+% Put a box around axes
+box on;
+% Put gridlines on the figure
+grid on;
+% Set axes limits
+axis(2*[-1 1 -1 1]);
+% Add title to the figure
+title({'Live position of 1-link-arm'});
+
 % Calculate cartesian coordinates for link
 X = cosd(q);
 Y = sind(q);
 
-% Clear the figure
-clf
+m = [0 X; 0 Y];
 
 % Plot the link
-plotv(X,Y,'-o');
+plotv(m,'-')
 
 % Update the plot
 drawnow;
+
+% Turn hold off
+hold off;
 
