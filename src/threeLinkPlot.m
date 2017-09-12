8b546029e74e50086dfb3b12898f90879c74903a
Nick Benoit <nbenoit@wpi.edu>
2017-09-12 00:53:13 -0400

    Began implementation of GUI

--- src/threeLinkPlot.m
+++ src/threeLinkPlot.m
@@ -15,13 +15,11 @@ y1 = [v0(2) v1(2)];
 z1 = [v0(3) v1(3)];
 
 % Second link
-%v2 = [(l2 * cosd(q1) * cosd(q0)) (l2 * cosd(q1) * sind(q0)) (l1 + l2 * sind(q1))];
 x2 = [v1(1) v2(1)];
 y2 = [v1(2) v2(2)];
 z2 = [v1(3) v2(3)];
 
 % Third link
-%v3 = pCoordinate(q0, q1, (q2 + 90));
 x3 = [v2(1) v3(1)];
 y3 = [v2(2) v3(2)];
 z3 = [v2(3) v3(3)];
@@ -56,11 +54,11 @@ plot3(x1, y1, z1, '-b', ...
 hold on;
 % Lock aspect ratios equal
 axis equal;
-% % Put a box around axes
+% Put a box around axes
 box on;
-% % Put gridlines on the figure
+% Put gridlines on the figure
 grid on;
-% % Set axes limits
+% Set axes limits
 axis([-2 2 -2 2 0 3]);
 % Add title and axes to the figure
 xlabel('X'); ylabel('Y'); zlabel('Z');
