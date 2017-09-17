5938d9c7e18f1551bc75dcb143571d0c1980e0af
Nick Benoit <nbenoit@wpi.edu>
2017-09-17 10:53:54 -0400

    Added threeLinkPlot with view select to GUI

--- src/gui.m
+++ src/gui.m
@@ -7,49 +7,44 @@ function gui
 f = figure('Visible','off','Position',[360,500,900,570]);
 
 % Construct the components
-hsurf    = uicontrol('Style','pushbutton',...
-            'String','Surf','Position',[315,220,70,25],...
-            'Callback',{@surfbutton_Callback});
-hmesh    = uicontrol('Style','pushbutton',...
-            'String','Mesh','Position',[315,180,70,25],...
-            'Callback',{@meshbutton_Callback});
-hcontour = uicontrol('Style','pushbutton',...
-            'String','Contour','Position',[315,135,70,25],...
-            'Callback',{@contourbutton_Callback});
+% hsurf   = uicontrol('Style','pushbutton',...
+%             'String','Surf','Position',[315,220,70,25],...
+%             'Callback',{@surfbutton_Callback});
+% hmesh    = uicontrol('Style','pushbutton',...
+%             'String','Mesh','Position',[315,180,70,25],...
+%             'Callback',{@meshbutton_Callback});
+% hcontour = uicontrol('Style','pushbutton',...
+%             'String','Contour','Position',[315,135,70,25],...
+%             'Callback',{@contourbutton_Callback});
 htext    = uicontrol('Style','text',...
-            'String','Select Data','Position',[325,90,60,15]);
-hpopup   = uicontrol('Style','popupmenu',...
-            'String',{'Peaks','Membrane','Sinc'},...
-            'Position',[300,50,100,25],...
+            'String','Select View','Position',[475,485,80,15]);
+hviewController = uicontrol('Style','popupmenu',...
+            'String',{'Iso','Top','Right','Front'},...
+            'Position',[475,450,100,25],...
             'Callback',{@popup_menu_Callback});
-% Add axes
-ha = axes('Units','pixels','Position',[50,60,200,185]);
+% Add axes and define placement of live plot
+ha = axes('Units','pixels','Position',[50,145,400,400]);
 % Align components along their centers
-align([hsurf, hmesh, hcontour, htext, hpopup], 'Center','None');
+%align([hsurf, hmesh, hcontour, htext, hviewController], 'Center','None');
+align([htext, hviewController], 'Center','None');
 
 % Initialize the UI.
 % Change units to normalized so components resize automatically.
 f.Units = 'normalized';
 ha.Units = 'normalized';
-hsurf.Units = 'normalized';
-hmesh.Units = 'normalized';
-hcontour.Units = 'normalized';
+% hsurf.Units = 'normalized';
+% hmesh.Units = 'normalized';
+% hcontour.Units = 'normalized';
 htext.Units = 'normalized';
-hpopup.Units = 'normalized';
+hviewController.Units = 'normalized';
 
-% Generate the data to plot.
-peaks_data = peaks(35);
-membrane_data = membrane;
-[x,y] = meshgrid(-8:.5:8);
-r = sqrt(x.^2+y.^2) + eps;
-sinc_data = sin(r)./r;
 
 % Create a plot in the axes.
-current_data = peaks_data;
-surf(current_data);
+threeLinkPlot(1, 1, eCoordinate(1, 1, 0, 0),...
+    pCoordinate(1, 1, 1, 0, 0, 0 + 90));
 
 % Assign a name to appear in the window title.
-f.Name = 'Simple GUI';
+f.Name = 'Arm Controller GUI';
 
 % Move the window to the center of the screen.
 movegui(f,'center')
@@ -61,36 +56,37 @@ f.Visible = 'on';
 %  determine which item is currently displayed and make it the
 %  current data. This callback automatically has access to 
 %  current_data because this function is nested at a lower level.
-   function popup_menu_Callback(source,eventdata) 
-      % Determine the selected data set.
-      str = source.String;
-      val = source.Value;
-      % Set current data to the selected data set.
-      switch str{val};
-      case 'Peaks' % User selects Peaks.
-         current_data = peaks_data;
-      case 'Membrane' % User selects Membrane.
-         current_data = membrane_data;
-      case 'Sinc' % User selects Sinc.
-         current_data = sinc_data;
-      end
+function popup_menu_Callback(source,eventdata) 
+   % Determine the selected data set.
+   str = source.String;
+   val = source.Value;
+   % Set current data to the selected data set.
+   switch str{val};
+   case 'Iso' % User selects Peaks.
+      view(37.5, 30);
+   case 'Top' % User selects Membrane.
+      view(0,90);
+   case 'Right' % User selects Sinc.
+      view(90,0);
+   case 'Front' % User selects
+      view(0,0);
    end
+end
 
 % Push button callbacks. Each callback plots current_data in the
 % specified plot type.
-
-function surfbutton_Callback(source,eventdata) 
-% Display surf plot of the currently selected data.
-     surf(current_data);
-end
-
-function meshbutton_Callback(source,eventdata) 
-% Display mesh plot of the currently selected data.
-     mesh(current_data);
-end
-
-function contourbutton_Callback(source,eventdata) 
-% Display contour plot of the currently selected data.
-     contour(current_data);
-end
+% function surfbutton_Callback(source,eventdata) 
+%     % Display surf plot of the currently selected data.
+%      surf(current_data);
+% end
+% 
+% function meshbutton_Callback(source,eventdata) 
+%     % Display mesh plot of the currently selected data.
+%      mesh(current_data);
+% end
+% 
+% function contourbutton_Callback(source,eventdata) 
+%     % Display contour plot of the currently selected data.
+%      contour(current_data);
+% end
 end
